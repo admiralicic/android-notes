c0b5e4a76fa3dc743bec53007819b0552a7fa280
Admir Alicic <admir.alicic@gmail.com>
2019-02-10 09:24:48 +0100

    Update to previous commit

--- app/src/main/java/com/admiralicic/notes/NotesListActivity.java
+++ app/src/main/java/com/admiralicic/notes/NotesListActivity.java
@@ -2,12 +2,21 @@ package com.admiralicic.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.admiralicic.notes.models.Note;
 
 public class NotesListActivity extends AppCompatActivity {
+
+    private static final String TAG = "NotesListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+
+        Note note = new Note("some title", "some content", "some timestamp");
+
+        Log.d(TAG, "onCreate: " + note.toString());
     }
 }
