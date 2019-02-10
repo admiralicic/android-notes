56a0dedbc25cb083f555422053de1858cea10817
Admir Alicic <admir.alicic@gmail.com>
2019-02-10 17:39:47 +0100

    Set up recycler view

--- app/src/main/java/com/admiralicic/notes/NotesListActivity.java
+++ app/src/main/java/com/admiralicic/notes/NotesListActivity.java
@@ -2,21 +2,54 @@ package com.admiralicic.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.LinearLayout;
 
+import com.admiralicic.notes.adapters.NotesRecyclerAdapter;
 import com.admiralicic.notes.models.Note;
+import com.admiralicic.notes.util.VerticalSpacingItemDecorator;
+
+import java.util.ArrayList;
 
 public class NotesListActivity extends AppCompatActivity {
 
     private static final String TAG = "NotesListActivity";
 
+    // UI components
+    private RecyclerView mRecyclerView;
+    // vars
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNotesRecyclerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+        mRecyclerView = findViewById(R.id.recyclerView);
 
-        Note note = new Note("some title", "some content", "some timestamp");
+        initRecyclerView();
+        insertFakeNotes();
+    }
 
-        Log.d(TAG, "onCreate: " + note.toString());
+    private void insertFakeNotes() {
+        for (int i = 0; i < 1000; i++) {
+            Note note = new Note();
+            note.setTitle("title # " + i);
+            note.setContent("content #: " + i);
+            note.setTimestamp("Jan 2019");
+            mNotes.add(note);
+        }
+        mNotesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
+        mNotesRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNotesRecyclerAdapter);
     }
 }
