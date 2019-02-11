daa03a36ab96c94e1178c1bcd675198abbbe2767
Admir Alicic <admir.alicic@gmail.com>
2019-02-11 20:55:05 +0100

    Add toolbar

--- app/src/main/java/com/admiralicic/notes/NotesListActivity.java
+++ app/src/main/java/com/admiralicic/notes/NotesListActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.LinearLayout;
 
@@ -31,6 +32,9 @@ public class NotesListActivity extends AppCompatActivity {
 
         initRecyclerView();
         insertFakeNotes();
+
+        setSupportActionBar((Toolbar)findViewById(R.id.notes_toolbar));
+        setTitle("Notes");
     }
 
     private void insertFakeNotes() {
